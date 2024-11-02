25580b1aced0d2d1f142b55c7c7600eac3eebe3d
Jason Yang <jzc.yang@mail.utoronto.ca>
2024-11-01 22:01:51 -0400

    Updated Downlaod Data Script

--- scripts/02-download_data.R
+++ scripts/02-download_data.R
@@ -11,16 +11,8 @@
 #### Workspace setup ####
 library(opendatatoronto)
 library(tidyverse)
-# 
-
-#### Download data ####
-
-
-
 
 #### Save data ####
-# 
-# change the_raw_data to whatever name you assigned when you downloaded it.
 write_csv(the_raw_data, "inputs/data/raw_data.csv") 
 
          
